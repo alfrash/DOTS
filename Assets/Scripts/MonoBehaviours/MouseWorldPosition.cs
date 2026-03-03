68768743495b4078d8e9aa15d963227916c5a914
alfrash <ahmed.alfrash@gmail.com>
2026-03-03 23:37:16 +0200

    using physics raycast to get MouseWorldPosition

--- Assets/Scripts/MonoBehaviours/MouseWorldPosition.cs
+++ Assets/Scripts/MonoBehaviours/MouseWorldPosition.cs
@@ -6,8 +6,9 @@ public class MouseWorldPosition : MonoBehaviour
     public Vector3 GetPosition() {
         // Create a ray from the camera through the mouse position
         Ray mouseCameraRay = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
-        
+
         // Perform a raycast to check for intersections with colliders in the scene
+        // If the ray hits a collider, return the point of intersection; otherwise, return Vector3.zero
         if (Physics.Raycast(mouseCameraRay, out RaycastHit hitInfo)) {
             return hitInfo.point;
         }else {
